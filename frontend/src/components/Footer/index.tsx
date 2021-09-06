11c79f630821cd0be1e0864ae9caa1c3af08747b
Emerson F. Talarico <emersonft@hotmail.com>
2021-09-06 20:17:26 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/emersonft" target="_blank" rel="noreferrer">Emerson Francisco Talarico</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/emerson-talarico-23b40723/" target="_blank" rel="noreferrer">Linkedin</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
